06d133db9c01bd5ee4fb46added426fb1709c658
Kalos Protocol <kaizendex@protonmail.com>
2023-02-23 23:46:54 +1100

    color

--- packages/uikit/src/theme/colors.ts
+++ packages/uikit/src/theme/colors.ts
@@ -41,7 +41,7 @@ export const lightColors: Colors = {
     bubblegum: "linear-gradient(139.73deg, #2e304d 50%, #23232b 100%)",
     inverseBubblegum: "linear-gradient(139.73deg, #F3EFFF 0%, #E5FDFF 100%)",
     cardHeader: "linear-gradient(111.68deg, #F2ECF2 0%, #E8F2F6 100%)",
-    blue: "linear-gradient(180deg, #A7E8F1 0%, #94E1F2 100%)",
+    blue: "linear-gradient(180deg, #16759d 0%, #94E1F2 100%)",
     violet: "linear-gradient(180deg, #E2C9FB 0%, #CDB8FA 100%)",
     violetAlt: "linear-gradient(180deg, #CBD7EF 0%, #9A9FD0 100%)",
     gold: "linear-gradient(180deg, #FFD800 0%, #FDAB32 100%)",
